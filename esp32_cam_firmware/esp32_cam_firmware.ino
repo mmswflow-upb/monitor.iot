ceed211df595a286f10710c85e5ffa2218a7a1c5
mmswflow-upb <mmswflow@gmail.com>
2024-12-07 04:15:52 +0200

    esp32-cam sets the color of RGB led correctly

--- esp32_cam_firmware/esp32_cam_firmware.ino
+++ esp32_cam_firmware/esp32_cam_firmware.ino
@@ -4,30 +4,49 @@
 #include "mbedtls/base64.h" // For Base64 decoding
 #include <WebSocketsClient_Generic.h> // For WebSocket connection
 
-/*
- WiEarth
-3TS9fFtZNf
-mmswflow@gmail.com
-123456
-http://monitor-iot-server-b4531a0ea68c.herokuapp.com
-ws://monitor-iot-server-b4531a0ea68c.herokuapp.com
- */
-
 const char* ssid = "WiEarth";           
-const char* password = "3TS9fFtZNf";   
-const String serverUrl = "https://monitor-iot-server-b4531a0ea68c.herokuapp.com";
+const char* password = "3TS9fFtZNf";  
+const String serverUrl = "http://monitor-iot-server-b4531a0ea68c.herokuapp.com";
 const String socketUrl = "wss://monitor-iot-server-b4531a0ea68c.herokuapp.com";
 
-const String deviceName = "esp32-Cam";
-const String deviceType = "sensor";
+// const char* ssid = "IoTProjectWifi";           
+// const char* password = "12345678";  
+// const String serverUrl = "http://127.0.0.1:8080";
+// const String socketUrl = "ws://127.0.0.1:8080";
+
+// Device Info
+const String deviceName = "ESP32-RGB-Lamp";
+const String deviceType = "RGB-Lamp";
+const String deviceId = "1bA";
+
+// RGB LED Pins (common anode)
+#define RED_PIN 15
+#define GREEN_PIN 2
+#define BLUE_PIN 14
+
+WebSocketsClient webSocket;
 
 String jwtToken;
 String userId;
 
-WebSocketsClient webSocket;
+// Default RGB values (initial values can be set to 0)
+int r = 0, g = 200, b = 0;
 
-#include <ArduinoJson.h>
+// Device data object (initialized before connecting to the server)
+StaticJsonDocument<512> deviceData;
 
+void setupDeviceData() {
+  // Initialize the device data with default values
+  deviceData["userId"] = "";  // Initially empty, will be set after login
+  deviceData["deviceId"] = deviceId;
+  deviceData["deviceName"] = deviceName;
+  deviceData["deviceType"] = deviceType;
+  deviceData["data"]["r"] = r;
+  deviceData["data"]["g"] = g;
+  deviceData["data"]["b"] = b;
+}
+
+// WebSocket event handler
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
   switch (type) {
     case WStype_DISCONNECTED:
@@ -36,37 +55,47 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
 
     case WStype_CONNECTED:
       Serial.println("WebSocket connected.");
-      webSocket.sendTXT("Hello from ESP32!");
+      // Send device information as soon as the connection is established
+      sendDeviceInfoToServer();
       break;
 
     case WStype_TEXT: {
-      Serial.print("WebSocket message received: ");
-      Serial.println((char *)payload);
+      
 
       StaticJsonDocument<512> jsonDoc;
       DeserializationError error = deserializeJson(jsonDoc, payload);
 
       if (!error) {
-        Serial.println("Parsed JSON Data:");
-        for (JsonPair keyValue : jsonDoc.as<JsonObject>()) {
-          String key = keyValue.key().c_str();
-          String value;
+        if(jsonDoc.containsKey("type")){
+          String pongMessage = "{\"type\": \"pong\", \"message\": \"pong\"}";
+          webSocket.sendTXT(pongMessage);  // Send pong back to server
           
-          // Determine the type of the value and handle it accordingly
-          if (keyValue.value().is<const char*>()) {
-            value = String(keyValue.value().as<const char*>());
-          } else if (keyValue.value().is<int>()) {
-            value = String(keyValue.value().as<int>());
-          } else if (keyValue.value().is<float>()) {
-            value = String(keyValue.value().as<float>());
-          } else if (keyValue.value().is<bool>()) {
-            value = keyValue.value().as<bool>() ? "true" : "false";
-          } else {
-            value = "Unknown type";
-          }
-
-          Serial.println("Key: " + key + ", Value: " + value);
+          break;
         }
+        // Check if the message contains RGB values
+        if(jsonDoc.containsKey("data")){
+          Serial.print("Device Object Updated: ");
+          Serial.println((char *)payload);
+          
+          
+            // Update the global RGB values
+            r = jsonDoc["data"]["r"].as<int>();
+            g = jsonDoc["data"]["g"].as<int>();
+            b = jsonDoc["data"]["b"].as<int>();
+
+            Serial.println("New RGB: " + String(r) + ", " + String(g) + ", " + String(b));
+
+            // Since it's a common anode RGB LED, invert the PWM values
+            analogWrite(RED_PIN, 255 - r);   // Invert red PWM value
+            analogWrite(GREEN_PIN, 255 - g); // Invert green PWM value
+            analogWrite(BLUE_PIN, 255 - b);  // Invert blue PWM value
+
+            // Update deviceData with new color values
+            deviceData["data"]["r"] = r;
+            deviceData["data"]["g"] = g;
+            deviceData["data"]["b"] = b;
+        }
+        
       } else {
         Serial.println("Failed to parse WebSocket message as JSON.");
         Serial.print("Error: ");
@@ -79,8 +108,6 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
       break;
   }
 }
-
-
 
 String base64UrlDecode(const String &input) {
   String decodedInput = input;
@@ -157,8 +184,23 @@ bool loginToServer() {
 
   userId = payloadDoc["id"].as<String>();
   Serial.println("User ID extracted: " + userId);
+  
+  // Update device data with userId after successful login
+  deviceData["userId"] = userId;
+
   return true;
 }
+
+void sendDeviceInfoToServer() {
+  // Convert device object to JSON string
+  String deviceJson;
+  serializeJson(deviceData, deviceJson);
+
+  // Send the device object via WebSocket
+  webSocket.sendTXT(deviceJson);
+  Serial.println("Device info sent to the server: " + deviceJson);
+}
+
 void connectToWebSocket() {
   String host = "monitor-iot-server-b4531a0ea68c.herokuapp.com"; // WebSocket host
   uint16_t port = 80;                  // Heroku uses port 80 for WebSocket connections
@@ -166,6 +208,7 @@ void connectToWebSocket() {
   String path = "/?token=" + jwtToken + 
                 "&userId=" + userId + 
                 "&type=mcu" + 
+                "&deviceId=" + deviceId +
                 "&deviceName=" + deviceName + 
                 "&deviceType=" + deviceType;
 
@@ -173,16 +216,29 @@ void connectToWebSocket() {
   webSocket.onEvent(webSocketEvent);
 }
 
-
 void setup() {
   Serial.begin(115200);
+
+  // Setup device data with default values before any connection
+  setupDeviceData();
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) delay(500);
 
-  if (loginToServer()) connectToWebSocket();
+  Serial.println("Connected to WiFi");
+
+  // Initialize RGB LED pins
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+ 
+
+  if (loginToServer()) {
+    connectToWebSocket(); 
+  }
 }
 
 void loop() {
-  webSocket.loop();
+  webSocket.loop();  // Listen for WebSocket messages
 }
